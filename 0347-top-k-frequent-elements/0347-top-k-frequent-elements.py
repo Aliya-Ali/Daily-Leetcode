7d32812256a6285bc36908c49b92a70e718d4350
Aliya-Ali <107311375+Aliya-Ali@users.noreply.github.com>
2024-06-22 19:53:02 +0500

    Time: 93 ms (35.51%), Space: 22.5 MB (5.53%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+class Solution:
+    # basket sort
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mp ={}
+        freq = [[] for i in range(len(nums)+1)]
+        res = []
+        # dictionary to count the number of occursion in nums
+        for i in nums:
+            mp[i] = 1 + mp.get(i, 0)
+        
+        # we make number of count an index  
+        for n, ind in mp.items():
+            freq[ind].append(n)
+        
+        print(freq)
+        #now reversing the frequent list and saving them to our list
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                #print(n)
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        
