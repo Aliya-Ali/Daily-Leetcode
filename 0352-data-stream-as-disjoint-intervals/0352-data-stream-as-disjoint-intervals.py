aa7b0de80ccf2c0eb3277c3105c15b2c748986f4
Aliya-Ali <107311375+Aliya-Ali@users.noreply.github.com>
2024-06-25 23:23:27 +0500

    Time: 46 ms (11.89%), Space: 17.3 MB (11.66%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -1,4 +1,22 @@
+from sortedcontainers import SortedDict
+
 class SummaryRanges:
+    def __init__(self):
+        self.treeMap = SortedDict()
+        
+    def addNum(self, value: int) -> None:
+        self.treeMap[value] = True
+        
+    def getIntervals(self) -> List[List[int]]:
+        res = []
+        for n in self.treeMap:
+            if res and res[-1][1] +1 == n:
+                res[-1][1] = n
+            else:
+                res.append([n,n])
+        return res
+        
+class SummaryRanges1:
 
     def __init__(self):
         self.ranges = []
