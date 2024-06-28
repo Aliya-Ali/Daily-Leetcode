8020997406acd662952df0b529c1a444c80fc217
Aliya-Ali <107311375+Aliya-Ali@users.noreply.github.com>
2024-06-28 16:58:20 +0500

    Time: 32 ms (77.23%), Space: 16.5 MB (85.46%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.py
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -1,5 +1,22 @@
 class Solution:
+    #breath first search
     def canMeasureWater(self, x: int, y: int, target: int) -> bool:
+        q = deque([0])
+        seen= set()
+        quantity = [x, y, -x, -y]
+        while q:
+            curr = q.popleft()
+            for i in quantity:
+                total = curr + i
+                if total == target:
+                    return True
+                if total not in seen and 0<= total <= x + y:
+                    q.append(total)
+                    seen.add(total)
+        return False
+    
+    # depth first search
+    def canMeasureWater1(self, x: int, y: int, target: int) -> bool:
         seen = set()
         def dfs(total):
             if total == target:
