84b524ef96a7adc8b87d0bf46315bb4dd779f342
Aliya-Ali <107311375+Aliya-Ali@users.noreply.github.com>
2024-08-20 23:57:40 +0500

    Time: 98 ms (88.51%), Space: 17.4 MB (50.72%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -1,30 +1,28 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        l = 0
-        max_count = count = 0
-        
-        for r in range(len(s)):
-            count += 1 if s[r] in "aeiou" else 0
-            if r - l + 1 > k:
-                count -= 1 if s[l] in "aeiou" else 0
-                l +=1
-            max_count = max(max_count, count)
-        return max_count
-        
-    def maxVowels1(self, s: str, k: int) -> int:
+        set_array = set(['a','e','i','o', 'u'])
         count = 0
-        for i in range(k):
-            if s[i] in "aeiou":
-                count +=1
-        max_count = count
-        
-        for i in range(len(s) -k):
-            if s[i] in "aeiou":
+        res =0
+        for i, e in enumerate(s):
+            if i>= k  and s[i-k] in set_array:
                 count -=1
-            if s[i+k] in "aeiou":
-                count += 1
-            
-            if count > max_count:
-                max_count = count
-        return max_count
-            
+            if e in set_array:
+                count +=1
+                res = max(res, count)
+        return res
+    
+    def maxVowels1(self, s: str, k: int) -> int:
+        l = 0
+        r = 0
+        set_array = set()
+        res = 0 
+        for r in range(len(s)):
+            if (r-l)> k:
+                set_array.remove(s[l])
+                l += 1
+            if s[r] in "aeiou":
+                set_array.add(s[r])
+                res = max(res, r - l + 1)
+        return res
+                
+        
