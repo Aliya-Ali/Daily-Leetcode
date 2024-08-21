406674f293363fe07380cbc458331cd93d62a756
Aliya-Ali <107311375+Aliya-Ali@users.noreply.github.com>
2024-08-21 23:58:12 +0500

    Time: 487 ms (14.03%), Space: 28 MB (77.97%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,24 +1,26 @@
 class Solution:
-    def containsNearbyDuplicate1(self, nums: List[int], k: int) -> bool:
-        mp = {}
-        for i in range(len(nums)):
-            if nums[i] in mp:
-                if i- mp[nums[i]] <= k:
-                    return True
-                mp[nums[i]] = i
-                
-            else:
-                mp[nums[i]] = i
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        left = 0
+        set_Array = set()
+        for right in range(len(nums)):
+            if (right - left) > k:
+                set_Array.remove(nums[left])
+                left+=1
+            if nums[right] in set_Array:
+                return True
+            set_Array.add(nums[right])
         return False
     
-    def containsNearbyDuplicate(self, nums: List[int], k: int)-> bool:
-        window = set()
-        for i in range(len(nums)):
-            if nums[i] in window: return True
-            
-            window.add(nums[i])
-            
-            if len(window) > k:
-                window.remove(nums[i-k])
+    def containsNearbyDuplicate1(self, nums: List[int], k: int) -> bool:
+        left = 0
+        for right in range(len(nums)): 
+            if right - left >k:
+                left += 1
+            if nums[right] == nums[left] and right != left:
+                return True
+            print(left)
+            print(right)
         return False
+            
+                
         
